8c974428aa8afd8ae7a58e353ce335f807a4d8f9
Giovane Delfino Junior <giovane.delfinojunior@gmail.com>
2021-07-28 10:04:23 -0300

    Cadastro de Categorias

---
+++ SistemaVendas/Models/CategoriaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Models
+{
+    public class CategoriaViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage = "Informe a Descrição da Categoria!")]
+
+        public string Descricao { get; set; }
+    }
+}
